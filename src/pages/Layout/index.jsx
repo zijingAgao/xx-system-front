485626f4e814326e1910261a07a052b109d6ace3
xiaohuan <1305584373@qq.com>
2024-02-13 16:59:33 +0800

    Layout结构搭建

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,5 +1,73 @@
-const Layout = () => {
-  return <div>this is Layout</div>
+import { Layout, Menu, Popconfirm } from "antd"
+import {
+  HomeOutlined,
+  DiffOutlined,
+  EditOutlined,
+  LogoutOutlined
+} from '@ant-design/icons'
+import './index.scss'
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
+
+const { Header, Sider} = Layout
+
+const items = [
+  {
+    label: '首页',
+    key: '/',
+    icon: <HomeOutlined />
+  },
+  {
+    label: '文章管理',
+    key: '/article',
+    icon: <DiffOutlined />
+  },
+  {
+    label: '创建文章',
+    key: '/publish',
+    icon: <EditOutlined />
+  }
+]
+
+const MyLayout = () => {
+  const navigate = useNavigate()
+
+  const onMenuClick = (route) => {
+    const path = route.key
+    navigate(path)
+  }
+
+  // 菜单反向高亮
+  const location = useLocation()
+  const selectedKey = location.pathname
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <div className="user-info">
+          <span className="user-name">username</span>
+          <span className="user-logout">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+              <LogoutOutlined /> 退出
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} theme="light" className="site-layout-background">
+          <Menu
+            mode="inline"
+            selectedKeys={selectedKey}
+            items={items}
+            onClick={onMenuClick}
+          />
+        </Sider>
+        <Layout className="layout-content" style={{padding: 20}}>
+          {/* 二级路由出口 */}
+          <Outlet />
+        </Layout>
+      </Layout>
+    </Layout>
+  )
 }
 
-export default Layout
+export default MyLayout
