b279ad17866b567ba05adfdc678880c75b6b1c75
xiaohuan <1305584373@qq.com>
2024-02-15 19:30:57 +0800

    配置接口跨域代理

--- craco.config.js
+++ craco.config.js
@@ -5,5 +5,16 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: 'http://1.14.64.132:9999',
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "/api"
+        }
+      }
+    }
   }
 }
