277f41b2d60b214969e3b94f7b709ecf74e6d549
xiaohuan <1305584373@qq.com>
2024-02-21 16:37:52 +0800

    打包优化-配置路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,30 +2,45 @@ import Layout from "../pages/Layout";
 import Login from "../pages/Login";
 
 import { createBrowserRouter } from "react-router-dom";
-import { AuthRoute } from '@/components/AuthRoute'
-import Home from "@/pages/Home";
-import UserManage from "@/pages/UserManage";
+import { AuthRoute } from "@/components/AuthRoute";
+import { Suspense, lazy } from "react";
+
+// 1.lazy函数对组件进行导入
+const Home = lazy(() => import("@/pages/Home"));
+const UserManage = lazy(() => import("@/pages/UserManage"));
 
 // 配置路由实例
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <AuthRoute><Layout /></AuthRoute>,
-        children: [
-            {
-                path: 'home',
-                element: <Home />
-            },
-            {
-                path: 'user',
-                element: <UserManage />
-            },
-        ]
-    },
-    {
-        path: "/login",
-        element: <Login />
-    },
-])
+  {
+    path: "/",
+    element: (
+      <AuthRoute>
+        <Layout />
+      </AuthRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={"加载中"}>
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: "user",
+        element: (
+          <Suspense fallback={"加载中"}>
+            <UserManage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
-export default router	
+export default router;
